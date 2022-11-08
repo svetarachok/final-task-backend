d8d5611d04df0b4b9009a4cda156da0cf037cb7d
Sveta Rachok <sveta.r1212@gmail.com>
2022-11-08 21:42:39 +0300

    feat: add mongo

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://pma-appU:EN6ZMQ5YFOjUkTwV@pma-cluster.emrbvj6.mongodb.net/final-task-backend');
+    await mongoose.connect('mongodb+srv://pma-appU:<password>@pma-cluster.emrbvj6.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
